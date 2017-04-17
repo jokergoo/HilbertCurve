5fa6fd36993ef90570d31199a4ec53c8f7bc243c
jokergoo <jokergoo@gmail.com>
2017-04-17 12:46:40 +0200

    generate figures

--- R/GenomicHilbertCurve.R
+++ R/GenomicHilbertCurve.R
@@ -474,7 +474,8 @@ setMethod(f = "hc_map",
 
 				hc2 = GenomicHilbertCurve(mode = "normal", chr = unique(as.vector(seqnames(background))), level = 6, newpage = FALSE)
 				hc_map(hc2, add = TRUE, labels = labels, fill = NA, border = NA)
-				# .ENV$I_PLOT = oi
+				seekViewport(name = paste0("hilbert_curve_", oi, "global"))
+				upViewport()
 			}
 		} else {
 			hc_polygon(object, background, gp = gpar(fill = fill, col = border))
